842756cd7751747dff44ed9bfbeec3d312022733
mhatzade@valethi.in <mhatzade@valethi.in>
2025-07-23 17:56:49 +0530

    some changes

--- react-router/src/Components/GitHub/Github.tsx
+++ react-router/src/Components/GitHub/Github.tsx
@@ -7,7 +7,7 @@ const Github = () => {
         <div className="max-w-lg w-full mx-auto flex items-center justify-between p-4 my-4 bg-yellow-200 rounded-md">
             <img src={data?.avatar_url} width={300} className="rounded-full" />
             <h2 className="text-xl font-[500]">{data?.name}</h2>
-        </div>
+        </div> 
     )
 }
 

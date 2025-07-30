5abdf607aa14ad68192728fbde0c28746ad09fb4
mhatzade@valethi.in <mhatzade@valethi.in>
2025-07-30 11:42:10 +0530

    react router

--- react-router/src/Components/Header/Header.tsx
+++ react-router/src/Components/Header/Header.tsx
@@ -1,5 +1,17 @@
 import { Link, NavLink } from 'react-router'
+import useTheme from '../../Context/ThemeContext.js'
 export default function Header() {
+
+    const { themeMode, lightMode, darkMode } = useTheme()
+
+    const onChangeBtn = (e:any) => {
+        const darkModeStatus = e.currentTarget.checked
+        if (darkModeStatus) {
+            darkMode()
+        } else {
+            lightMode()
+        }
+    }
 
     const menuList = [
         { name: 'Home', path: '' },
@@ -25,7 +37,7 @@ export default function Header() {
                             to="#"
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
-                            Log in
+                            Log in 
                         </Link>
                         <Link
                             to="#"
@@ -33,6 +45,17 @@ export default function Header() {
                         >
                             Get started
                         </Link>
+                        <label className="relative inline-flex items-center cursor-pointer">
+                            <input
+                                type="checkbox"
+                                value=""
+                                className="sr-only peer"
+                                onChange={onChangeBtn}
+                                checked={themeMode=== "dark"}
+                            />
+                            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                            <span className="ml-3 text-sm font-medium text-gray-900">Toggle Theme</span>
+                        </label>
                     </div>
                     <div
                         className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
--- react-router/src/Layout.tsx
+++ react-router/src/Layout.tsx
@@ -1,13 +1,31 @@
 import { Header, Footer} from './Components'
 import { Outlet } from 'react-router'
+import { ThemeProvider  } from './Context/ThemeContext.js'
+import { useEffect, useState } from 'react'
 
 const Layout = () => {
+
+    const [themeMode, setThemeMode] = useState<any>('light')
+
+    const darkMode = () => {
+        setThemeMode('dark')
+    }
+
+    const lightMode = () => {
+        setThemeMode('light')
+    }
+
+    // useEffect(() => {
+    //     document.querySelector('html')?.classList.remove("light", 'dark')
+    //     document.querySelector('html')?.classList.add(themeMode)
+    // }, [themeMode])
+
     return(
-        <>
+        <ThemeProvider value={{themeMode, darkMode, lightMode}}>
             <Header />
             <Outlet />
             <Footer />
-        </>
+        </ThemeProvider>
     )
 }
 
